38319f66afd70d29411bc4cf2cce3b975543efe6
heo <admin@admin.com>
2025-04-07 00:20:34 +0900

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import { router } from './router/Router'
 import { RouterProvider } from 'react-router-dom'
 
+
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
   <RouterProvider router={router} />,
