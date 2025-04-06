af90b08dc0a32032123d26063354c4a285021653
heo <admin@admin.com>
2025-04-07 00:18:44 +0900

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { router } from './router/Router'
+import { RouterProvider } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,4 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
+import MainPage from '../ui/pages/MainPage'
+
 
 const routes = [
   {
